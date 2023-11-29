1a80182a70f6343ef7272a0376e047158fd229ae
yanyi <yanyi9399@outlook.com>
2023-11-29 23:37:38 +0800

    update AWS SDK for JavaScript to v3

--- app/utils/s3.server.ts
+++ app/utils/s3.server.ts
@@ -2,13 +2,12 @@ import {
 	unstable_parseMultipartFormData,
 	UploadHandler,
 } from "@remix-run/node";
-import S3 from 'aws-sdk/clients/s3.js';
+import { GetObjectCommand, PutObjectCommand, S3Client  } from "@aws-sdk/client-s3";
 import cuid from "cuid";
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const s3 = new S3({
+const client = new S3Client({
 	region: process.env.KUDOS_BUCKET_REGION,
-	accessKeyId: process.env.KUDOS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.KUDOS_SECRET_ACCESS_KEY,
 });
 
 async function convertToBuffer(data: AsyncIterable<Uint8Array>) {
@@ -19,21 +18,29 @@ async function convertToBuffer(data: AsyncIterable<Uint8Array>) {
 	return Buffer.concat(result);
 }
 
+
 const uploadHandler: UploadHandler = async ({ name, filename = '', contentType, data }) => {
-	const stream = await convertToBuffer(data);
-	
 	if (name !== "profile-pic") {
 		return undefined;
 	}
-	
-	const { Location } = await s3
-		.upload({
-			Bucket: process.env.KUDOS_BUCKET_NAME || "",
-			Key: `${cuid()}.${filename.split(".").slice(-1)}`,
-			Body: stream,
-		})
-		.promise();
-	return Location;
+
+	const config = {
+		Bucket: process.env.KUDOS_BUCKET_NAME || "",
+		Key: `${cuid()}.${filename.split(".").slice(-1)}`,
+	}
+	const getObjectCommand = new GetObjectCommand(config);
+	const putObjectCommand = new PutObjectCommand({
+	    ...config,
+		Body: await convertToBuffer(data)
+	});
+	try {
+		await client.send(putObjectCommand);
+		const signedUrl = await getSignedUrl(client, getObjectCommand);
+		
+		return signedUrl.split('?')[0];
+	  } catch (err) {
+		return null;
+	  }
 };
 
 export async function uploadAvatar(request: Request) {
